e0e0286c68ba0f87338429fec1cf74d3a7a09a9f
Renato Crivano <renato.crivano@gmail.com>
2016-11-03 12:57:44 -0200

    Acrescentando dependência de Internet.

--- src/main/java/com/crivano/bluc/rest/server/BluCServlet.java
+++ src/main/java/com/crivano/bluc/rest/server/BluCServlet.java
@@ -1,6 +1,8 @@
 package com.crivano.bluc.rest.server;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.crivano.blucservice.api.IBlueCrystal;
 import com.crivano.swaggerservlet.SwaggerServlet;
+import com.crivano.swaggerservlet.SwaggerUtils;
+import com.crivano.swaggerservlet.dependency.TestableDependency;
 
 public class BluCServlet extends SwaggerServlet {
 	private static final long serialVersionUID = 1756711359239182178L;
@@ -20,16 +24,29 @@ public class BluCServlet extends SwaggerServlet {
 		super.setAPI(IBlueCrystal.class);
 
 		super.setActionPackage("com.crivano.bluc.rest.server");
+
+		addDependency(new TestableDependency("network", "internet", false) {
+			String testsite = "http://www.google.com";
+
+			@Override
+			public String getUrl() {
+				return testsite;
+			}
+
+			@Override
+			public boolean test() throws Exception {
+				final URL url = new URL(testsite);
+				final URLConnection conn = url.openConnection();
+				conn.connect();
+				return true;
+			}
+
+		});
+
 	}
 
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doPost(request, response);
-	}
-
-	@Override
-	protected String getService() {
-		return "blue-crystal";
 	}
 }
